c3b6f11e4d70a5810d3ac73349c1b79bbaa31265
Thiago Silva <thyagosousasilva@gmail.com>
2021-12-23 14:33:24 -0300

    Adicionando implementação sem aplicação do padrão comportamental State

---
+++ comportamentais/4-state/1-pedido/src/main/java/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+        // Criando um pedido
+        Pedido p = new Pedido();
+
+        // Tentando mudar o status do pedido para enviado imediatamente
+        p.despacharPedido();
+
+        // Informando que o status do pedido é PAGO
+        p.sucessoAoPagar();
+        p.sucessoAoPagar();
+
+        // Cancelando o pedido
+        p.cancelarPedido();
+
+        // Tentando enviar o pedido com status CANCELADO
+        p.despacharPedido();
+    }
+}
